947bd3249502e0d43af6a7c25c6f32991537ec21
pablo.5892 <pablo.5892314@gmail.com>
2023-02-24 16:14:47 -0300

    commit de modulo 3

---
+++ variables.js
@@ -0,0 +1,28 @@
+var cajon1;
+
+// string
+
+var bootcamp = "Henry"
+
+// Numeros
+
+var numeroEntero = 17;
+
+var numeroNegativo = -17;
+
+var decimales = 17.7;
+
+//Booleanos
+
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+
+// Undefined
+
+var cajon1;
+
+var console.log(cajon1);
+
+// Null
+
+var sinNada = null;
